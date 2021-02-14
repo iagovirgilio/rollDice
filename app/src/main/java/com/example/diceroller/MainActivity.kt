25d741481415267691a5f6ee0fc2112261524b97
Iago Virgílio <iagovirgilio@gmail.com>
2021-02-13 23:39:59 -0300

    Add images dices

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
-import kotlin.random.Random as RandomRandom
 
 class MainActivity : AppCompatActivity() {
 
