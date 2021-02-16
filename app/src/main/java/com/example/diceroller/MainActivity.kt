2d9ccf186dc9c1ae5464c7b58e9c69cfda611236
Iago Virgílio <iagovirgilio@gmail.com>
2021-02-15 21:34:07 -0300

    Refactored code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
 
-        val randomInt = Random().nextInt(6) + 1
-        val drawbleResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -34,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        diceImage.setImageResource(drawbleResource)
+        diceImage.setImageResource(drawableResource)
 
     }
 }
